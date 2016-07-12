ccdb9c2be2a9b94d354c257505a197bdd0739f6e
volcanodeng <2767726116@qq.com>
2016-07-12 23:01:01 +0800

    录凭证

--- Scripts/App/Voucher.App.js
+++ Scripts/App/Voucher.App.js
@@ -21,8 +21,6 @@ function loadVoucher()
             $("#voucherTab").datagrid("enableCellEditing");
         }
     });
-
-    
 }
 
 
@@ -63,50 +61,81 @@ function onDateChange(newValue, oldValue)
         $("#vch_period").text(m);
     }
 }
+function datagridEndEdit()
+{
+    var dg = $("#voucherTab");
+    var editInd = dg.datagrid('options').editIndex;
+    if (editInd == undefined) return true;
 
+    if (dg.datagrid('validateRow', editInd)) {
+        if (typeof (fieldSettingFun) == "function") fieldSettingFun(editInd);
+
+        dg.datagrid('endEdit', editInd);
+        dg.datagrid('options').editIndex = undefined;
+
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 function saveVoucher()
 {
-    $.post("/api/v/SaveVoucher",
+    if (datagridEndEdit()) {
+        var voucherDetails = [];
+        var changeRows = $("#voucherTab").datagrid("getChanges");
+        if (changeRows.length == 0)
         {
-            VId:1,
-            CertWord: $("#cw").combobox("getValue"),
-            CertWordSn: $("#ss").numberspinner('getValue'),
-            AttachedInvoices: $("#attCw").numberbox('getValue'),
-            VoucherTime:$("#dd").datebox("getValue"),
-            VoucherDetails: [
-                {
-                    VId: 1,
-                    Abstracts: '测试凭证',
-                    SubjectCode: 1002,
-                    Debit: 30,
-                    Credit:0,
-                    Quantity:0
-                },
-                {
-                    VId: 1,
-                    Abstracts: '测试1',
-                    SubjectCode: 1404,
-                    Debit: 0,
-                    Credit: 30,
-                    Quantity: 0
+            $.messager.show({ title: '保存', msg: '没有修改内容' });
+            return;
+        }
+
+        $.post("/api/v/SaveVoucher",
+            {
+                VId: 1,
+                CertWord: $("#cw").combobox("getValue"),
+                CertWordSn: $("#ss").numberspinner('getValue'),
+                AttachedInvoices: $("#attCw").numberbox('getValue'),
+                VoucherTime: $("#dd").datebox("getValue"),
+                VoucherDetails: changeRows
+                    //[
+                    //{
+                    //    VId: 1,
+                    //    Abstracts: '测试凭证',
+                    //    SubjectCode: 1002,
+                    //    Debit: 30,
+                    //    Credit: 0,
+                    //    Quantity: 0
+                    //},
+                    //{
+                    //    VId: 1,
+                    //    Abstracts: '测试1',
+                    //    SubjectCode: 1404,
+                    //    Debit: 0,
+                    //    Credit: 30,
+                    //    Quantity: 0
+                    //}
+                    //]
+            },
+            function (data) {
+                if (data >= 1) {
+                    $.messager.show({ title: '保存', msg: '保存操作成功完成' });
                 }
-            ]
-        },
-        function (data) {
-            if(data>=1)
-            {
-                $.messager.show({title:'保存',msg:'保存操作成功完成'});
-            }
-            else
-            {
-                $.messager.alert('保存','保存失败，请联系管理员处理','warning');
-            }
-        });
+                else {
+                    $.messager.alert('保存', '保存失败，请联系管理员处理', 'warning');
+                }
+            });
+    }
 }
 function addVoucher()
 {
-
+    if(datagridEndEdit())
+    {
+        var dg = $("#voucherTab");
+        dg.datagrid('appendRow', { abstracts: '', subjectCode: '', subjectName: '', debit: 0, credit: 0 });
+        
+    }
 }
 
 function fieldSettingFun(editIndex)
